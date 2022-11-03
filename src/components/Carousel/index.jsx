4cbc9e5b35271d619fb8970e8955480cb33fb309
pvianap <pedro.v.parente@gmail.com>
2022-11-03 14:29:57 +0100

    Carousel first version

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import arrowLeft from '../../assets/arrow_left.svg';
+import arrowRight from '../../assets/arrow_right.svg';
+import './carousel.scss';
 
-export default function Carousel() {
-  return <p>Carousel</p>;
+export default function Carousel({ data }) {
+  const [current, setCurrent] = useState(0);
+  const slides = data;
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className="carousel">
+      <img
+        src={arrowLeft}
+        className="carousel__arrowLeft"
+        alt="flèche gauche"
+        onClick={prevSlide}
+      />
+      <img
+        src={arrowRight}
+        className="carousel__arrowRight"
+        alt="flèche droite"
+        onClick={nextSlide}
+      />
+
+      {slides.map((slide, index) => {
+        return (
+          <div
+            className={
+              index === current ? 'carousel__slide--active' : 'carousel__slide'
+            }
+            key={index}
+          >
+            {index === current && (
+              <img src={slide} className="carousel__image" alt="slide" />
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
 }
--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -1,12 +1,14 @@
 import { React, Fragment } from 'react';
-
+import Carousel from '../../components/Carousel';
 import { useLocation } from 'react-router-dom';
 import './lodging.scss';
 export default function Lodging() {
   const data = useLocation().state;
   return (
     <Fragment>
-      <h1>Logement {data.title}</h1>
+      <Carousel data={data.pictures} />
+      <h1>{data.title}</h1>
+      <p>{data.location}</p>
     </Fragment>
   );
 }
